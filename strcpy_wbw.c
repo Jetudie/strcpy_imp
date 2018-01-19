bcc92f4adbbd07b8dc4213aa985215d5e8c7d83b
Jetudie <fxm40007@hotmail.com>
2018-01-19 21:29:00 +0800

    Make strcpy_wbw.c always copy word by word

--- strcpy_wbw.c
+++ strcpy_wbw.c
@@ -1,8 +1,8 @@
 #include "strcpy_imp.h"
 
 /* Nonzero if either X or Y is not aligned on a "long" boundary.  */
-#define UNALIGNED(X, Y) \
-  (((long)X & (sizeof (long) - 1)) | ((long)Y & (sizeof (long) - 1)))
+#define UNALIGNED(X) \
+  (((long)X & (sizeof (long) - 1)) )
 
 #if LONG_MAX == 2147483647L
 #define DETECTNULL(X) (((X) - 0x01010101) & ~(X) & 0x80808080)
@@ -13,29 +13,27 @@
 #endif
 
 char* strcpy(char* dst0, const char* src0){
-  char *dst = dst0;
-  char *src = src0;
-  long *aligned_dst;
-  long *aligned_src;
+	char *dst = dst0;
+	char *src = src0;
+	long *dstl;
+	long *srcl;
 
-  /* If SRC or DEST is unaligned, then copy bytes.  */
-  if (!UNALIGNED (src, dst))
-    {
-      aligned_dst = (long*)dst;
-      aligned_src = (long*)src;
+	for(int i = 0; i < UNALIGNED(dst0); ++i){
+		*dst++ = *src++;
+	}
+	
+	/* If SRC or DEST is aligned, then copy four-byte wise.  */
+	dstl = (long*)dst;
+	srcl = (long*)src;
 
-      /* SRC and DEST are both "long int" aligned, try to do "long int"
-         sized copies.  */
-      while (!DETECTNULL(*aligned_src))
-        {
-          *aligned_dst++ = *aligned_src++;
-        }
+	/* SRC and DEST are both "long int" aligned, try to do "long int" sized copies.  */
+	while (!DETECTNULL(*srcl)){
+		*dstl++ = *srcl++;
+	}
 
-      dst = (char*)aligned_dst;
-      src = (char*)aligned_src;
-    }
-
-  while ((*dst++ = *src++))
-    ;
-  return dst0;
+	dst = (char*)dstl;
+	src = (char*)srcl;
+	while(*dst++ = *src++)
+		;
+	return dst0;
 }
