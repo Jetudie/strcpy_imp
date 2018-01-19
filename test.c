07b118706223f13424cff6b74c120746a7cb3a43
Jetudie <fxm40007@hotmail.com>
2018-01-19 20:27:41 +0800

    Add time struct in test.c

--- test.c
+++ test.c
@@ -1,12 +1,32 @@
 #include<stdio.h>
 #include<string.h>
 #include<limits.h>
+#include<time.h>
 #include"strcpy_imp.h"
 
+/* timing helper function */
+static double tvgetf(void){
+	struct timespec ts;
+	double sec;
+
+	clock_gettime(CLOCK_REALTIME, &ts);
+	sec = ts.tv_nsec;
+	sec /= 1e9;
+	sec += ts.tv_sec;
+
+	return sec;
+}
+
 int main(){
+	double t1, t2;
 	char text[] = "Let it go. Let it go. Can't hold it back anymore";
 	char text1[100];
 
+
+	t1 = tvgetf();
 	printf("%s\n", strcpy(text1, text));
+	t2 = tvgetf();
+
+	printf("Complete in %f sec.\n", t2-t1);
 	return 0;
 }
