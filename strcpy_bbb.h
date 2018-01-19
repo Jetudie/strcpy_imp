136cd5baa164e511192e98f9ac313d31c6ebd50e
Jetudie <fxm40007@hotmail.com>
2018-01-19 13:48:35 +0800

    Add header files

---
+++ strcpy_bbb.h
@@ -0,0 +1,10 @@
+#ifdef STRCPY_BBB_H_
+#define STRCPY_BBB_H_
+
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+
+char* strcpy_bbb(char*, const char*);
+
+#endif
---
+++ strcpy_wbw.h
@@ -0,0 +1,10 @@
+#ifdef STRCPY_WBW_H_
+#define STRCPY_WBW_H_
+
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+
+char* strcpy_wbw(char*, const char*);
+
+#endif
